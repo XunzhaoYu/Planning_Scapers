b3f73c879e2b7bda878e2d9aae8bd114899a8eb5
Xunzhao Yu <yuxunzhao@gmail.com>
2026-04-05 06:19:30 +0100

    update Tascomi_scraper - Wirral. Add url_preprocess for Wirral scraper.

--- UKPlanning/scrapers/Tascomi_scraper.py
+++ UKPlanning/scrapers/Tascomi_scraper.py
@@ -50,7 +50,7 @@ class Tascomi_Scaper(Base_Scraper):
         super().__init__(*args, **kwargs)
 
         # All sub_classes of Base_Scraper should define their self.parse_func(s) in __init__
-        if self.auth in ['Coventry', 'Harrow', 'Breckland', 'Rother']:
+        if self.auth in ['Coventry', 'Harrow', 'Breckland', 'Rother', 'Wirral']:
             self.url_check = True
             self.url_preprocess = self.url_preprocess_Tascomi
         elif self.auth in ['Gwynedd']:
@@ -59,9 +59,10 @@ class Tascomi_Scaper(Base_Scraper):
             self.parse_func = self.parse_data_item_Tascomi
 
     LA_url_dict = {'Breckland': 'https://publicportal.breckland.gov.uk/planning/index',
-                   'Coventry': 'https://planandregulatory.coventry.gov.uk/planning/index',
+                   'Coventry':  'https://planandregulatory.coventry.gov.uk/planning/index',
                    'Harrow':    'https://planningsearch.harrow.gov.uk/planning/index',
-                   'Rother':    'https://online.rother.gov.uk/planning/index', }
+                   'Rother':    'https://online.rother.gov.uk/planning/index',
+                   'Wirral':    'https://online.wirral.gov.uk/planning/index'}
 
     # Note: these item names are ending with ':'.
     details_dict = {'Application Reference Number:': 'uid',
