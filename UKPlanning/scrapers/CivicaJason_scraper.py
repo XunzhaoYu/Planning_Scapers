5ae58f514217b5ff575e2153539a46f6a3cb304d
Xunzhao Yu <yuxunzhao@gmail.com>
2026-04-18 06:23:54 +0100

    update CivicaJason scraper - general. Add data.

--- UKPlanning/scrapers/CivicaJason_scraper.py
+++ UKPlanning/scrapers/CivicaJason_scraper.py
@@ -44,7 +44,47 @@ class CivicaJason_Scraper(Base_Scraper):
         # All sub_classes of Base_Scraper should define their self.parse_func(s) in __init__
         self.parse_func = self.parse_data_item_CivicaJason
 
-    # details_dict
+    details_dict = {'Reference Number': 'uid', # StAlbans
+                    'Case No': 'uid', # Waverley
+
+                    'Application Description': 'description',  # Eastbourne
+                    'Proposal': 'description',  # Waverley
+                    'Application Type': 'other_fields.application_type',  # Denbighshire, Eastbourne, StAlbans, Waverley
+
+                    'Premises Address': 'address', # Ashfield, Denbighshire, Eastbourne
+                    'Gaz Address': 'address', # Waverley
+                    'Location': 'address', # StAlbans
+
+                    'Applicant Name': 'other_fields.applicant_name', # Ashfield, StAlbans, Waverley
+                    'Applicant': 'other_fields.applicant_name', # Denbighshire, Eastbourne
+                    'Agent': 'other_fields.agent_name',  # All
+                    'Case Officer': 'other_fields.case_officer',  # All
+
+                    'Ward': 'ther_fields.ward_name',  # All
+                    'Parish': 'other_fields.parish',  # All
+                    'Decision Date': 'other_fields.decision_issued_date', # All
+                    'Decision': 'other_fields.decision',  # All
+
+                    'Received Date': 'other_fields.date_received', # StAlbans
+                    'Date Valid': 'other_fields.date_validated', # Ashfield, Denbighshire, Eastbourne, StAlbans
+                    'Date Advertised': 'other_fields.last_advertised_date', # StAlbans
+                    #'Application Site Visit Date': '', # StAlbans
+                    'Stage': 'other_fields.status', # Denbighshire, Eastbourne
+                    'Committee Date': 'other_fields.meeting_date', # StAlbans
+                    'Target Determination Date': 'other_fields.determination_date', # Eastbourne
+                    'Decision Due By': 'other_fields.decision_due_date', # Ashfield, Denbighshire, Waverley
+                    'Please Comment By': 'other_fields.comment_expires_date', # Denbighshire, Eastbourne, StAlbans
+                    'Comments due by date': 'other_fields.comment_expires_date', # Waverley
+                    'Expiry Date': 'other_fields.application_expires_date', # StAlbans
+                    'Decision Level': 'other_fields.expected_decision_level', # StAlbans
+
+                    #'Appeal Date': '', # Waverley
+                    'Appeal Lodged Date': 'other_fields.appeal_lodged_date', # StAlbans
+                    'Appeal Reference': 'other_fields.appeal_reference', # StAlbans
+                    #'Appeal Method': '', # StAlbans
+                    'Appeal Decision Date': 'other_fields.appeal_decision_date', # StAlbans
+                    'Appeal Status': 'other_fields.appeal_status', # Denbighshire, Eastbourne, StAlbans, Waverley
+                    }
 
     def create_item(self, driver, folder_name, file_urls, document_names):
         if not os.path.exists(self.failed_downloads_path + folder_name):
@@ -68,11 +108,39 @@ class CivicaJason_Scraper(Base_Scraper):
         print(f'parse_data_item_CivicaJason, scraper name: {scraper_name}, max_file_name_len: {max_file_name_len}.')
 
         try:
-            tab_panel = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="content"]/div/section')))
+            content = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@role='tablist']/div/div")))
+            """
+            1 //*[@id="applicationviewer"]/div/div/div
+            2 //*[@id="applicationviewer"]/div/div/div
+            3 //*[@id="applicationSummary"]/div/div/div
+            4 //*[@id="civica-planningsearchandview"]/div/div[2]/div/div/div/div
+            5 //*[@id="civica-planningsearchandview"]/div/div[2]/div/div/div/div
+            """
         except TimeoutException:
             # Planning Application details not available.
             note = response.xpath('//*[@id="main-content"]/article/h1/text()').get()
             print('note: ', note)
             return
 
+        tab_list = content.find_elements(By.XPATH, "./div[@role='tab']")
+        tab_panel_list = content.find_elements(By.XPATH, "./div[@role='tab-panel']")
+
+        for tab_index, tab in enumerate(tab_list):
+            tab_name = tab.find_element(By.XPATH, './div').text.strip()
+            if 'false' in tab.get_attribute('aria-expanded'):
+                print(f'panel {tab_name} expanded: false.')
+                tab.click()
+                time.sleep(2)
+            # --- --- --- Details (data) --- --- ---
+            if 'detail' in tab_name.lower():
+                pass
+            # --- --- --- Documents (doc) --- --- ---
+            elif 'document' in tab_name.lower():
+                pass
+            # --- --- --- Comments () --- --- ---
+            elif 'comment' in tab_name.lower():
+                pass
+            else:
+                print(f'\n{tab_index + 1}. Unknown Tab: {tab_name}.')
+                assert 1 == 0
         self.ending(app_df)
